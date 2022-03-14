678bb80a4826442397027076b22ff8227f3fccac
SAYANDEEP ADHIKARY <93661020+sayandeep-adhikary@users.noreply.github.com>
2022-03-14 07:54:04 +0530

    atm.c
    this is a simple replica of ATM machines.

--- atm.c
+++ atm.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
-    int card, lang, bal, amount, sel, pin;
+    int card, lang, bal, amount, sel, pin1, pin2, pin;
     printf("PLEASE INSERT YOUR CARD\n");
     printf("If you have entered your card, enter 1\n");
     scanf("%d", &card);
@@ -18,50 +18,77 @@ int main(int argc, char const *argv[])
         case 4:
             printf("You have choosen Hindi\n");
             break;
-        
+
         default:
             printf("ERROR! Enter card again\n");
             break;
         }
     }
-    printf("1. Balance Check\n\n2. Withdrawl\n\n3. Set PIN\n");
-    scanf("%d", &bal);
-    switch (bal)
-    {
-    case 1:
-        printf("YOUR ACCOUNT BALANCE IS 100,000 $");
-        break;
-    case 2:
-        printf("Enter the amount\n");
-        scanf("%d", &amount);
-        if (amount>=100000)
+    label:
+        printf("1. Balance Check\n\n2. Withdrawl\n\n3. Set PIN\n");
+        scanf("%d", &bal);
+        switch (bal)
         {
-            printf("ERROR! YOU DO NOT HAVE ENOUGH BALANCE! TRY AGAIN!");
+        case 1:
+            printf("YOUR ACCOUNT BALANCE IS 100,000 $\nPress 1 to go back to previous menu or 0 to exit\n");
+            scanf("%d", &bal);
+            if (bal == 1)
+            {
+                goto label;
+            }
+            else if (bal == 0)
+            {
+                printf("Thank you! Have a nice day");
+                goto end;
+            }
+            break;
+
+        case 2:
+            amount1:
+                printf("Enter amount\n");
+                scanf("%d", &amount);
+                if (amount > 100000)
+                {
+                printf("YOU DO NOT HAVE ENOUGH BALANCE IN YOUR ACCOUNT! PLEASE TRY AGAIN!\n");
+                goto amount1;
+                }
+                else printf("1. Savings\n\n2. Current\n");
+            scanf("%d", &sel);
+            switch (sel)
+            {
+            case 1:
+                printf("You have selected Savings account\n\nEnter your pin now.\n***MAKE SURE NO ONE CAN SEE YOUR PIN***\n");
+                break;
+            case 2:
+                printf("You have selected Current account\n\nEnter your pin now.\n***MAKE SURE NO ONE CAN SEE YOUR PIN***\n");
+                break;
+            }
+            break;
+        case 3:
+            pin3:
+                printf("Enter your pin : ");
+                scanf("%d", &pin1);
+                printf("Enter again to confirm : ");
+                scanf("%d", &pin2);
+                if (pin1==pin2)
+                {
+                    printf("PIN SET SUCCESSFULLY!");
+                    goto end;
+                }
+                else if (pin1!=pin2)
+                
+                {
+                    printf("ERROR! PIN DID NOT MATCH!!! TRY AGAIN!\n");
+                    goto pin3;
+                }
+                goto end;
+        
+        
         }
-        else
-            printf("YOU HAVE ENTERED '%d $'\n", amount);
-    }
-    printf("Press 1 to continue and 0 to exit : ");
-    scanf("%d", &amount);
-    if (amount==1)
-    {
-        printf("1. Savings\n\n2. Current\n");
-    }
-    if (amount==0)
-    {
-        printf("Thank you! Have a nice day!");
-    }
-    scanf("%d", &sel);
-    switch (sel)
-    {
-    case 1:
-        printf("Enter your pin now.\n***MAKE SURE NO ONE CAN SEE YOUR PIN***\n");
-        break;
-    case 2:
-        printf("Enter your pin now.\n***MAKE SURE NO ONE CAN SEE YOUR PIN***\n");
-        break;
-    }
     scanf("%d", &pin);
-    printf("You have entered ****\nPlese wait...\n\n\nTRANSACTION SUCCESSFUL! TAKE YOUR CARD OUT");
-    return 0;
+    printf("YOU HAVE ENTERED ****\nPlease wait...\n\n\nTRANSACTION SUCCESSFUL! TAKE YOUR CARD OUT.\n");
+    printf("Available balance = %d", 100000-amount);
+    end:
+        return 0;
 }
+
